dd39479ee6eb708aaaf07108543988d99d11710d
abdelrahman-ahmed-nassar <aanassar.io@gmail.com>
2026-04-19 17:44:00 +0200

    Remove AppHeader component and its usage from App

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,4 @@
 import { Navigate, Route, Routes } from "react-router-dom";
-import AppHeader from "./components/AppHeader";
 import ChaptersIndexPage from "./pages/ChaptersIndexPage";
 import ReadPage from "./pages/ReadPage";
 
@@ -9,8 +8,6 @@ const App = () => {
       className="flex h-screen min-h-dvh flex-col overflow-hidden min-[900px]:flex-row"
       dir="rtl"
     >
-      <AppHeader />
-
       <main className="min-w-0 flex-1 overflow-y-auto p-5 pb-21.5 min-[900px]:px-5 min-[900px]:pb-5">
         <Routes>
           <Route path="/" element={<ChaptersIndexPage />} />
