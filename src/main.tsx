f37e42adc4a2463cb290713371a0e024b554f2fd
abdelrahman-ahmed-nassar <aanassar.io@gmail.com>
2026-04-19 17:38:57 +0200

    Set basename for BrowserRouter to support routing

--- src/main.tsx
+++ src/main.tsx
@@ -16,7 +16,7 @@ registerSW({
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename={import.meta.env.BASE_URL}>
       <App />
     </BrowserRouter>
   </StrictMode>,
